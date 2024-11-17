724636aa44fb13903031f43d9cc21add9de13b40
1thiagoCRUZ <thiagocruz58452@gmail.com>
2024-11-17 16:21:33 -0300

    Adicionando cors para conseguir realizar requisições na API

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); 
 const app = express();
 
 const taskUsers = require("./routes/taskRoutes");
 const notesUsers = require("./routes/notesRoutes");
 
-app.use(express.json());
 
+app.use(cors()); 
+
+app.use(express.json());
 
 app.use("/", taskUsers);
 app.use("/", notesUsers);
 
-app.listen(process.env.APP_PORT, ()=>{
-    console.log("Servidor rodando na porta:", process.env.APP_PORT)
-})
+app.listen(process.env.APP_PORT, () => {
+    console.log("Servidor rodando na porta:", process.env.APP_PORT);
+});
